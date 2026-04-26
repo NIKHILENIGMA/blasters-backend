433e90f11b5cd9ef631fcf6bef4d774803375f00
NIKHILENIGMA <nickharmalkar18@gmail.com>
2026-04-26 07:07:53 -0700

    feat: Add franchise routes to the main router

--- src/core/http/router.ts
+++ src/core/http/router.ts
@@ -4,6 +4,7 @@ import PlayerRoutes from '@/modules/players/players.route'
 import TeamRoutes from '@/modules/team/team.routes'
 import AdminRoutes from '@/modules/admin/admin.routes'
 import UserRoutes from '@/modules/user/user.routes'
+import FranchiseRoutes from '@/modules/franchise/franchise.routes'
 
 const router = Router()
 
@@ -11,5 +12,6 @@ router.use('/players', PlayerRoutes)
 router.use('/team', TeamRoutes)
 router.use('/admin', AdminRoutes)
 router.use('/users', UserRoutes)
+router.use('/franchise', FranchiseRoutes)
 
 export default router
